ed5f80040899a8ddd8f64d99f08bb9fe0b102261
respawm1 <respawm19@gmail.com>
2022-02-21 14:55:32 +0000

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const { config } = require('process');
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './tailwind.config.js'},
+    autoprefixer: {},
+
+  }
+}
